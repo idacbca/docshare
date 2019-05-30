cb7802d1d23ffdba9bed763113b9c995b3d7cd54
LYY16110117 <785931921@qq.com>
2019-05-30 15:08:52 +0800

    test2

--- home/index/controller/upload.php
+++ home/index/controller/upload.php
@@ -157,8 +157,8 @@ class upload extends Common
 	//添加商品函数
 	public function goods_add(){
 		$goods = db('goods');
+		$file = getfile();
 		$goods->data([
-            'title'  =>  $_POST['title'],
             'goodsname' => $_POST['goodsname'],
 			'goodtype' => $_POST['goodtype'],
 			'categoryid' => $_POST['categoryid'],
@@ -171,24 +171,8 @@ class upload extends Common
 		]);
     
     // 移动到框架应用根目录/public/uploads/ 目录下
-        if($data['file']){
-            $info = $data['file']->move(ROOT_PATH . 'public' . DS . 'uploads');
-            if($info){
-            // 成功上传后 获取上传信息
-            // 输出 jpg
-            echo $info->getExtension();
-            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
-            echo $info->getSaveName();
-            // 输出 42a79759f284b767dfcb2a0197904287.jpg
-            echo $info->getFilename(); 
-            }else{
-            // 上传失败获取错误信息
-            echo $file->getError();
-            }
-        }
 		$result = $goods->save();
 		// $result = $db->insert($data);
-        
 		if($result){
 			$this->success('商品添加成功！', 'product_list');
 		} else{
