9ca175a888d3ecff052738bfd6c5722c75b423f5
LYY16110117 <785931921@qq.com>
2019-05-30 17:57:37 +0800

    更新

--- home/index/controller/Index.php
+++ home/index/controller/Index.php
@@ -10,7 +10,7 @@ class Index extends Common
     {
         if(input('id')){
         $goods = db('goods');
-        $data = $goods->where('id', input('id'))->select();
+        $data = $goods->where('goodsid', input('id'))->select();
         $this->assign([
             'title' => 'DocShare', 
             'details' => $data 
@@ -38,11 +38,12 @@ class Index extends Common
     public function blog_details()
     {
         if(input('id')){
-            //var_dump(input('id'));
+            
             //$type = $this->getCatgory();
 
             $goods = db('goods');
-            $data = $goods->where('id', input('id'))->select();
+            $data = $goods->where('goodsid', input('id'))->select();
+            //var_dump($data);
             $this->assign([
                 'details' => $data,
             ]);
@@ -58,4 +59,52 @@ class Index extends Common
             return $this->fetch();
       }
   }
+  public function goods_add()
+  {
+    $goods = db('goods');
+    $goods->data([
+        'goodsname' => $_POST['goodsname'],
+        'goodtype' => $_POST['goodtype'],
+        'categoryid' => $_POST['categoryid'],
+        //'brief' => $_POST['brief'],
+        'unitprice' => $_POST['unitprice'],
+        'quantity' => $_POST['quantity'],
+        'freight' => $_POST['freight'],
+       // 'filepath' => $_POST['filepath'],
+        //'filepath' => implode(',', $_POST['imagepath']),
+    ]);
+    var_dump($goods);
+    $result = $goods->save();
+    // $result = $db->insert($data);
+    if($result){
+        $this->success('商品添加成功！', 'upload');
+    } else{
+        $this->error('商品添加失败！');
+    }
+}
+
+public function download(){
+        $famlePath = input('filepath');
+        $file_dir = ROOT_PATH . 'public' . DS . 'uploads' . '/' . "$famlePath";    // 下载文件存放目录
+        
+        // 检查文件是否存在
+        if (! file_exists($file_dir) ) {
+            $this->error('文件未找到');
+        }else{
+            // 打开文件
+            $file1 = fopen($file_dir, "r");
+            // 输入文件标签
+            Header("Content-type: application/octet-stream");
+            Header("Accept-Ranges: bytes");
+            Header("Accept-Length:".filesize($file_dir));
+            Header("Content-Disposition: attachment;filename=" . $file_dir);
+            ob_clean();     // 重点！！！
+            flush();        // 重点！！！！可以清除文件中多余的路径名以及解决乱码的问题：
+            //输出文件内容
+            //读取文件内容并直接输出到浏览器
+            echo fread($file1, filesize($file_dir));
+            fclose($file1);
+            exit();
+        }
+}
 }
--- home/index/controller/upload.php
+++ home/index/controller/upload.php
@@ -3,157 +3,11 @@ namespace app\index\controller;
 
 use think\Controller;
 use think\org\Upload;
-class upload extends Common
+class Upload extends Common
 {	
-	// //商品管理页
-	// public function product_list(){
-	// 	// $start_time = '';
-	// 	// $end_time = '';
-	// 	if($_POST){//输入查询信息时
-	// 		$search = $_POST['search'];
-	// 		$word=$search;
-	// 		$goods = model('goods');
-	// 		$data = $goods->where('goodsname','like','%'.$search.'%')->select();
-	// 		$count = count($data);
-	// 		$this->assign([
-	// 			'data' => $data,
-	// 			'count' => $count,
-	// 			'word' => $word
-	// 		]);
-	// 		return $this->fetch();
-	// 	} else{//没有输入查询信息时
-	// 		$goods = model('goods');
-	// 		$data = $goods->select();
-	// 		$count = count($data);
-	// 		$this->assign([
-	// 			'data' => $data,
-	// 			'count' => $count
-	// 		]);
-	// 		return $this->fetch();
-	// 	}
-	// }
-
-	// // 删除商品ajax
-	// public function product_del_ajax(){
-	// 	$id = $_POST['id'];
-	// 	$db = db('goods');
-		
-	// 	$path= $db->where('id',$id)->column('filepath');
-	// 	$images=explode(',',$path);
-	// 	$file= db('goods_files');
-	// 	foreach($images as $v){
-    //        $file->where('id'.$v)->delete();
-    //     }
-		
-	// 	$re = $db->where('id', $id)->delete();
-	// 	if($re){
-	// 		echo 1;
-	// 	}else echo 0;
-	// }
-
-	// // 商品上架ajax
-	// public function product_start_ajax(){
-	// 	$id = $_POST['id'];
-	// 	$db = db('goods');
-	// 	$re = $db->where('id', $id)->update(['status' => '0']);
-	// 	if($re){
-	// 		echo 1;
-	// 	}else echo 0;
-	// }
-
-	// // 商品下架ajax
-	// public function product_stop_ajax(){
-	// 	$id = $_POST['id'];
-	// 	$db = db('goods');
-	// 	$re = $db->where('id', $id)->update(['status' => '1']);
-	// 	if($re){
-	// 		echo 1;
-	// 	}else echo 0;
-	// }
-
+	
 	// 编辑商品页面
-	public function product_edit(){
-		$data = db('goods_type')->field("*, concat(path, ',', id) as paths")->order('paths')->select();
-		foreach ($data as $k => $v) {
-			$data[$k]['name'] = str_repeat("|------", $v['level']).$v['name'];
-		}
-		$this->assign('data', $data);
-
-		
-		$db = db('goods');
-		$id = input('param.id');
-		$goods = $db->find($id);
-
-		$images=explode(',',$goods['filepath']);
-		$i=model('GoodsFiles');
-        $image=[];
-        foreach($images as $v){
-           array_push($image,$i->find($v));
-        }
-
-		
-		$this->assign('goods', $goods);
-		
-		$this->assign('image',$image);
-		return $this->fetch();
-	}
-
-	//修改商品信息函数
-	public function goods_edit(){
-		$goods = model('goods');
-		$tid = explode(",", $_POST['tid']);
-		
-		if(isset($_POST['imagepath'])){
-				$result = $goods->save([
-				'goodsname'  =>  $_POST['goodsname'],
-				'tid' => $tid[0],
-				'tpid' => $tid[1],
-				'unit' => $_POST['unit'],
-				'attributes' => $_POST['attributes'],
-				'number' => $_POST['number'],
-				'curprice' => $_POST['curprice'],
-				'cosprice' => $_POST['cosprice'],
-				'inventory' => $_POST['inventory'],
-				'freight' => $_POST['freight'],
-				'status' => $_POST['status'],
-				'text' => $_POST['editorValue'],
-				'filepath' =>implode(',', $_POST['imagepath'])
-			],['id' => $_POST['id']]);
-		} else{
-			$result = $goods->save([
-				'goodsname'  =>  $_POST['goodsname'],
-				'tid' => $tid[0],
-				'tpid' => $tid[1],
-				'unit' => $_POST['unit'],
-				'attributes' => $_POST['attributes'],
-				'number' => $_POST['number'],
-				'curprice' => $_POST['curprice'],
-				'cosprice' => $_POST['cosprice'],
-				'inventory' => $_POST['inventory'],
-				'freight' => $_POST['freight'],
-				'status' => $_POST['status'],
-				'text' => $_POST['editorValue']
-			],['id' => $_POST['id']]);
-
-		}
-
-		if($result){
-			$this->success('商品修改成功！', 'product_list');
-		} else{
-			$this->error('商品修改失败！');
-		}
-	}
-
-	// 商品添加页
-	// public function product_add(){
-	// 	$data = db('goods_type')->field("*, concat(path, ',', id) as paths")->order('paths')->select();
-	// 	foreach ($data as $k => $v) {
-	// 		$data[$k]['name'] = str_repeat("|------", $v['level']).$v['name'];
-	// 	}
-	// 	$this->assign('data', $data);
-	// 	return $this->fetch();
-	// }
-
+	
 	//添加商品函数
 	public function goods_add(){
 		$goods = db('goods');
@@ -181,12 +35,7 @@ class upload extends Common
 	}
 
 	// 商品分类页
-	public function product_category(){
-		return $this->fetch();
-	}
-
-	
-	public function upload(){
+	public function uploadfile(){
 		 // 获取表单上传文件
 		 $files = request()->file();
 		 foreach($files as $file){
