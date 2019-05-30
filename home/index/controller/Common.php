9c28a5b12d4b939ebb96bbd5f0680c6b3dc0e5a8
Winniewy <914386610@qq.com>
2019-05-30 14:57:02 +0800

    详情页显示数据库内容

--- home/index/controller/Common.php
+++ home/index/controller/Common.php
@@ -17,4 +17,37 @@ class Common extends Controller
         }
         
     }
+    
+    
+    public function getGoodsPath(){
+        //$m = db('goods_type');
+        $n = db('goods');
+        //$id = $n->where('id',input('id'))->value('tid');
+        //$idpath = $m->where('id',$id)->value('path');
+      //  $idpath = $m->where('id',$id)->value('path');
+        //$arr2 = explode(',',$idpath);
+       // $arr = array_filter($arr2);
+        $pathname2 = array();
+        foreach($arr as $k=>&$pathname){           
+            $arr[$k] = $m->where('id',$pathname)->value('name');               
+        }
+        //var_dump($arr);
+        return $arr;
+    }
+    public function getimgPath(){
+        $n = db('goods');
+        //$m = db('goods_files');
+        $img = $n->where('id',input('id'))->value('filepath');
+        $img2 = explode(',',$img);
+        //var_dump($img2);
+        $imgpath = array();
+        foreach($img2 as $img3){
+            $imgpath2 = $m->where('id',$img3)->select();
+            //var_dump($imgpath2);
+            array_push($imgpath,$imgpath2);
+        }
+        
+       //var_dump($imgpath);
+       return $imgpath;
+    }
 }
--- home/index/controller/Index.php
+++ home/index/controller/Index.php
@@ -2,21 +2,19 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
 
 class Index extends Common
 {
     public function index()
     {
+        
+        $total=Session::get('total'); 
         $this->assign([
-            'title' => 'DocShare'
-        ]);
-        return $this->fetch();
-    }
-
-    public function blog_details()
-    {
-        $this->assign([
-            'title' => 'DocShare - 详情'
+            'title' => 'DocShare',
+            'type' => $type,
+            'allgoods'=> $allgoods,
+            'total'=>$total
         ]);
         return $this->fetch();
     }
@@ -28,4 +26,28 @@ class Index extends Common
         ]);
         return $this->fetch();
     }
+
+    public function blog_details()
+    {
+        if(input('id')){
+            //var_dump(input('id'));
+            //$type = $this->getCatgory();
+
+            $goods = db('goods');
+            $data = $goods->where('id', input('id'))->select();
+            $this->assign([
+                'details' => $data,
+            ]);
+            return $this->fetch();
+        } else{
+            $goods = db('goods');
+            //$idpath = $this->getGoodsPath();
+           // $img = $this->getimgPath();
+            $data = $goods->select();
+            $this->assign([
+                'details' => $data,
+            ]);
+            return $this->fetch();
+      }
+  }
 }
